1ec5a69ed0745e494b8dae0bcd66d9ea6c2d5aa3
Tri <triphan921@gmail.com>
2021-04-30 13:53:17 -0700

    CRU

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,56 @@
 class SongsController < ApplicationController
+before_action :find_song, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @songs = Song.all
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    if @song
+      render :show
+    else
+      redirect_to(songs_path(@songs))
     end
+  end
 
-    def show
-        @song = Song.find_by(id: 
-        params[:id]
-        )
-        if @song
-            render :show
-        else
-            redirect_to(model_path(@songs))
-        end
-    end
+  def new
+    @song = Song.new
+  end
 
+  def create
+      @song = Song.new(song_params)
+      if @song.save
+        flash[:success] = "Song successfully created"
+        redirect_to @song
+      else
+        flash[:error] = "Something went wrong"
+        render 'new'
+      end
+  end
+  
+
+  def edit
+  end
+  
+  def update
+      if @song.update_attributes(song_params)
+        flash[:success] = "Song was successfully updated"
+        redirect_to @song
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+
+  private
+
+  def find_song
+    @song = Song.find_by(id: params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
-  belongs_to :artist, :dependent => :delete
-  belongs_to :genre, :dependent => :delete
-  # belongs_to :artist, class_name: "artist", foreign_key: "artist_id"
-  # belongs_to :genre, class_name: "genre", foreign_key: "genre_id"
+  # belongs_to :artist, :dependent => :delete
+  # belongs_to :genre, :dependent => :delete
+  belongs_to :artist, class_name: "artist", foreign_key: "artist_id"
+  belongs_to :genre, class_name: "genre", foreign_key: "genre_id"
 end
